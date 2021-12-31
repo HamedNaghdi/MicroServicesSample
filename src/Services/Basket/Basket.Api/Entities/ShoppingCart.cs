a468989e28de29f0da9cf27d47b7337bf926cd14
Hamed Naghdi <hamed.naghdi@hotmail.com>
2021-12-31 21:11:23 +0330

    3.7. Developing Basket.API Microservices Creating Entities

---
+++ src/Services/Basket/Basket.Api/Entities/ShoppingCart.cs
@@ -0,0 +1,25 @@
+﻿namespace Basket.Api.Entities;
+
+public class ShoppingCart
+{
+    public string Username { get; set; }
+    public List<ShoppingCartItem> Items { get; set; } = new ();
+
+    public decimal TotalPrice
+    {
+        get
+        {
+            return Items.Sum(item => item.Quantity * item.Price);
+        }
+    }
+
+    public ShoppingCart()
+    {
+        
+    }
+
+    public ShoppingCart(string username)
+    {
+        Username = username;
+    }
+}
