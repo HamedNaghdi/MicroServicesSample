37d3342dee8359d388ab78baeefdbfe2271f4504
Hamed Naghdi <hamed.naghdi@hotmail.com>
2022-02-26 00:52:13 +0330

    3.9. Developing Repository Pattern on Basket.API Microservice

---
+++ src/Services/Basket/Basket.Api/Repositories/IShoppingCartRepository.cs
@@ -0,0 +1,10 @@
+﻿using Basket.Api.Entities;
+
+namespace Basket.Api.Repositories;
+
+public interface IShoppingCartRepository
+{
+    Task<ShoppingCart?> GetCart(string username);
+    Task<ShoppingCart?> UpdateCart(ShoppingCart cart);
+    Task DeleteCart(string username);
+}
---
+++ src/Services/Basket/Basket.Api/Repositories/ShoppingCartRepository.cs
@@ -0,0 +1,48 @@
+﻿using Basket.Api.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+
+namespace Basket.Api.Repositories;
+
+public class ShoppingCartRepository : IShoppingCartRepository
+{
+    #region Fields
+
+    private readonly IDistributedCache _redisCache;
+
+    #endregion
+
+    #region Ctor
+
+    public ShoppingCartRepository(IDistributedCache redisCache)
+    {
+        _redisCache = redisCache ?? throw new ArgumentNullException(nameof(redisCache));
+    }
+
+
+
+    #endregion
+
+    #region Methods
+
+    public async Task<ShoppingCart?> GetCart(string username)
+    {
+        var cart = await _redisCache.GetStringAsync(username);
+        return string.IsNullOrEmpty(cart) ? null : JsonConvert.DeserializeObject<ShoppingCart>(cart);
+    }
+
+    public async Task<ShoppingCart?> UpdateCart(ShoppingCart cart)
+    {
+        if (cart is null)
+            throw new ArgumentNullException(nameof(cart));
+        await _redisCache.SetStringAsync(cart.Username, JsonConvert.SerializeObject(cart));
+        return await GetCart(cart.Username);
+    }
+
+    public async Task DeleteCart(string username)
+    {
+        await _redisCache.RemoveAsync(username);
+    }
+
+    #endregion
+}
