a436cb40ada24d4860be164a49678c78da2dd79e
Hamed Naghdi <hamed.naghdi@hotmail.com>
2022-04-14 18:59:08 +0430

    4.11. Test and Run Discount Microservice

--- src/Services/Discount/Discount.Api/Repositories/DiscountRepository.cs
+++ src/Services/Discount/Discount.Api/Repositories/DiscountRepository.cs
@@ -24,7 +24,8 @@ namespace Discount.Api.Repositories
         public async Task<Coupon> GetDiscount(string productName)
         {
             using var connection = new NpgsqlConnection(_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
-            var coupon = await connection.QueryFirstOrDefaultAsync<Coupon>("SELECT * FROM Coupon WEHERE ProductName = @ProductName", new { ProductName = productName });
+            var coupon = await connection.QueryFirstOrDefaultAsync<Coupon>
+                ("SELECT * FROM Coupon WHERE ProductName = @ProductName", new { ProductName = productName });
 
             return coupon ?? new Coupon { ProductName = "No Discount", Amount = 0, Description = "No Discount Desc" };
         }
@@ -39,10 +40,7 @@ namespace Discount.Api.Repositories
                     ("INSERT INTO Coupon (ProductName, Description, Amount) VALUES (@ProductName, @Description, @Amount)",
                             new { ProductName = coupon.ProductName, Description = coupon.Description, Amount = coupon.Amount });
 
-            if (affected == 0)
-                return false;
-
-            return true;
+            return affected != 0;
         }
 
         public async Task<bool> UpdateDiscount(Coupon coupon)
@@ -53,10 +51,7 @@ namespace Discount.Api.Repositories
                     ("UPDATE Coupon SET ProductName=@ProductName, Description = @Description, Amount = @Amount WHERE Id = @Id",
                             new { ProductName = coupon.ProductName, Description = coupon.Description, Amount = coupon.Amount, Id = coupon.Id });
 
-            if (affected == 0)
-                return false;
-
-            return true;
+            return affected != 0;
         }
 
         public async Task<bool> DeleteDiscount(string productName)
@@ -66,10 +61,7 @@ namespace Discount.Api.Repositories
             var affected = await connection.ExecuteAsync("DELETE FROM Coupon WHERE ProductName = @ProductName",
                 new { ProductName = productName });
 
-            if (affected == 0)
-                return false;
-
-            return true;
+            return affected != 0;
         }
     }
 }
