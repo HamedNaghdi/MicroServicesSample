be2a794013d2fe4496b47408fb30bf93e5a289df
Hamed Naghdi <hamed.naghdi@hotmail.com>
2022-04-16 19:32:32 +0430

    5.9. Implementing AutoMapper into DiscountService Class of Discount Grpc Microservice

--- src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -1,4 +1,5 @@
-﻿using Discount.Grpc.Protos;
+﻿using AutoMapper;
+using Discount.Grpc.Protos;
 using Discount.Grpc.Repositories;
 using Grpc.Core;
 
@@ -9,15 +10,17 @@ namespace Discount.Grpc.Services
         #region Fields
 
         private readonly IDiscountRepository _repository;
+        private readonly IMapper _mapper;
         private readonly ILogger<DiscountService> _logger;
 
         #endregion
 
         #region Ctor
 
-        public DiscountService(IDiscountRepository repository, ILogger<DiscountService> logger)
+        public DiscountService(IDiscountRepository repository, IMapper mapper, ILogger<DiscountService> logger)
         {
             _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
@@ -33,13 +36,16 @@ namespace Discount.Grpc.Services
             if (coupon is null)
                 throw new RpcException(new Status(statusCode: StatusCode.NotFound, $"Discount with product name = \"{request.ProductName}\" not found."));
 
-            return new CouponModel
-            {
-                Id = coupon.Id,
-                ProductName = coupon.ProductName,
-                Description = coupon.Description,
-                Amount = coupon.Amount
-            };
+            //return new CouponModel
+            //{
+            //    Id = coupon.Id,
+            //    ProductName = coupon.ProductName,
+            //    Description = coupon.Description,
+            //    Amount = coupon.Amount
+            //};
+            //_logger.LogInformation("Discount is retrieved for ProductName : {productName}, Amount : {amount}", coupon.ProductName, coupon.Amount);
+            _logger.LogInformation($"Discount is retrieved for ProductName : {coupon.ProductName}, Amount {coupon.Amount}");
+            return _mapper.Map<CouponModel>(coupon);
         }
 
         #endregion
