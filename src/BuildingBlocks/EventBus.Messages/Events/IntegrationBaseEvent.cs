931c295aed200f91d6b459866e984c190d78b720
Hamed Naghdi <hamed.naghdi@hotmail.com>
2022-05-05 00:27:40 +0430

    8.7. Developing BuildingBlocks EventBus.Messages Class Library

---
+++ src/BuildingBlocks/EventBus.Messages/Events/IntegrationBaseEvent.cs
@@ -0,0 +1,19 @@
+﻿namespace EventBus.Messages.Events;
+
+public class IntegrationBaseEvent
+{
+    public IntegrationBaseEvent()
+    {
+        Id = Guid.NewGuid();
+        CreationTime = DateTime.UtcNow;
+    }
+
+    public IntegrationBaseEvent(Guid id, DateTime createTime)
+    {
+        Id = id;
+        CreationTime = createTime;
+    }
+
+    public Guid Id { get; private set; }
+    public DateTime CreationTime { get; private set; }
+}
