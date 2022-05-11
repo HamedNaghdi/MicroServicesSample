71b033a8f4d62b6f0eb053085a40fc973647018c
Hamed Naghdi <hamed.naghdi@hotmail.com>
2022-05-11 14:25:38 +0430

    10.5. Developing Dto Model Class for Api Aggreation Operations

---
+++ src/ApiGateWays/Shopping.Aggregator/Models/CartModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Shopping.Aggregator.Models;
+
+public class CartModel
+{
+    public string Username { get; set; }
+    public List<CartItemExtendedModel> Items { get; set; } = new List<CartItemExtendedModel>();
+    public decimal TotalPrice { get; set; }
+}
